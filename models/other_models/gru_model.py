e46e4e842a28914ef8a419a29d42f4d274467853
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-07 08:52:12 -0800

    Update gru_model.py

--- models/other_models/gru_model.py
+++ models/other_models/gru_model.py
@@ -1,1 +1,16 @@
+import torch.nn as nn
+
+class GRUModel(nn.Module):
+    """
+    A simple GRU for sequence prediction.
+    """
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(GRUModel, self).__init__()
+        self.gru = nn.GRU(input_dim, hidden_dim, layer_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        out, _ = self.gru(x)
+        out = self.fc(out[:, -1, :])
+        return out
 
