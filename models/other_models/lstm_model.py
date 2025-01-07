59c56c307def6063f4183c02bcb18daf171b7f4c
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-07 08:51:33 -0800

    Update lstm_model.py

--- models/other_models/lstm_model.py
+++ models/other_models/lstm_model.py
@@ -1,1 +1,16 @@
+import torch.nn as nn
+
+class LSTMModel(nn.Module):
+    """
+    A simple LSTM for sequence prediction.
+    """
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(LSTMModel, self).__init__()
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])
+        return out
 
